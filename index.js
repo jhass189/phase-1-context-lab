a97cb857ddebd818e8a5a3c16ddd4c0c1e5b3113
James Hassett <jhassett189@gmail.com>
2022-06-29 20:55:04 -0400

    commit tree 46aa85b7695d67acc85c46e41056d53812f4bb79

--- index.js
+++ index.js
@@ -1,4 +1,47 @@
 /* Your Code Here */
+function createEmployeeRecord([firstName, familyName, title, payRatePerHour]) {
+    return {
+        firstName: firstName,
+        familyName: familyName,
+        title: title,
+        payPerHour: payRatePerHour,
+        timeInEvents: [],
+        timeOutEvents: [],
+    }
+}
+function createEmployeeRecords(arrayOfArrays) {
+    return arrayOfArrays.map((employee) => createEmployeeRecord(employee))
+}
+function createTimeInEvent(dateStamp) {
+    const saveDate = dateStamp.split(" ")
+    const timeIn = {
+        type: "TimeIn",
+        hour: parseInt(saveDate[1]),
+        date: saveDate[0]
+    }
+    this.timeInEvents.push(timeIn)
+    return this
+}
+function createTimeOutEvent(dateStamp) {
+    const saveDate = dateStamp.split(" ")
+    const timeOut = {
+        type: "TimeOut",
+        hour: parseInt(saveDate[1]),
+        date: saveDate[0]
+    }
+    this.timeOutEvents.push(timeOut)
+    return this
+}
+function hoursWorkedOnDate(dateStamp) {
+    const timeIn = this.timeInEvents.find(object => object.date === dateStamp)
+    const timeOut = this.timeOutEvents.find(object => object.date === dateStamp)
+    return (timeOut.hour - timeIn.hour) / 100
+}
+function wagesEarnedOnDate(dateStamp) {
+    const hours = hoursWorkedOnDate.call(this, dateStamp)
+    const payRate = this.payPerHour
+    return hours * payRate
+}
 
 /*
  We're giving you this function. Take a look at it, you might see some usage
@@ -19,5 +62,14 @@ const allWagesFor = function () {
     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
 
     return payable
+}
+
+function findEmployeeByFirstName(srcArray, theNameWereLookingFor) {
+    return srcArray.find((obj) => obj.firstName === theNameWereLookingFor)
+}
+
+function calculatePayroll(employeeArray) {
+    const payDates = employeeArray.reduce((prev, next) => prev + allWagesFor.call(next), 0)
+    return payDates
 }
 
